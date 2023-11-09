53241bc0827686c65bf7c5e4f9fa5c3eb2f882c9
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-09 11:20:54 +0000

    last update

--- next.config.js
+++ next.config.js
@@ -18,14 +18,7 @@ const nextConfig = {
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
             : "/api/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
-      },
+      }
     ];
   },
 };
