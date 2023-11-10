73e625b680e2e946ed843c1138cf46ad8a5d4a18
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-10 19:56:42 +0000

    quick

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
         source: "/api/v1:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api:path*"
-            : "/api/"
+            ? "http://127.0.0.1:8000/api/v1:path*"
+            : "/api/v1:path*",
       },
       {
         source: "/docs",
