217cbcc453658d7e77779dbab05b7125b0dab5dc
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-10 19:05:59 +0000

    quick

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/v1/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
+            ? "http://127.0.0.1:8000/api/v1:path*"
             : "/api/",
       },
       {
