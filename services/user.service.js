e6f64c6bee53f8f1001fb473dda8dc40d7617144
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-10 23:32:16 +0000

    quick

--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,7 @@
-import { baseService } from './base';
+import { baseService } from "./base";
+import axios from "axios";
 
-const baseUrl = '/users';
+const baseUrl = "/users";
 
 const getAll = async (options = {}) => {
   return await baseService(options).get(`${baseUrl}`);
@@ -11,9 +12,12 @@ const getOneById = async (id, options = {}) => {
 };
 
 const getCurrent = async (options = {}) => {
-  const {serverSide} = options;
-  if (serverSide){
-    return await axios.get(`http//:localhost:8000/api/v1/users/me`, { headers: {...options}, withCredentials: true });
+  const { serverSide } = options;
+  if (serverSide) {
+    return await axios.get(`http//:localhost:8000/api/v1/users/me`, {
+      headers: { ...options },
+      withCredentials: true,
+    });
   }
   return await baseService(options).get(`${baseUrl}/me`);
 };
