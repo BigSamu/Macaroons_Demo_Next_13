6f6b3447dc5b9c3af94e6b612a40af9eccd0361e
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-13 11:28:07 +0000

    quick

--- api/index.py
+++ api/index.py
@@ -82,7 +82,7 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 app.mount("/static/images", StaticFiles(directory="api/static/images"), name="images")
 
 #  Make static images available -> Authentication + Serverless Deployment (works for development too)
-if settings.ENVIRONMENT !== "development"
+if settings.ENVIRONMENT != "development":
     @app.get("api/v1/static/images/{file_name}")
     def serve_image(file_name: str, current_user: models.User = Depends(get_current_user)):
 
